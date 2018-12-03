ebcf19a95dea46d7a0e6e6a85262e5d26f0e41db
yxiawen <yxiawen@126.com>
2018-12-03 17:47:30 +0800

    12.3

--- koa/testapp/app.js
+++ koa/testapp/app.js
@@ -9,33 +9,37 @@ const controller = require('./controller');
 const bodyParser = require('koa-bodyparser')
 const router = require('koa-router')()
 const app = new Koa()
+const nunjucks = require('nunjucks');
 
-// app.use(async (ctx, next) => {
-//     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-//     await next();
-// })
+function createEnv(path, opts) {
+    var autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
 
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = `<h1>Index</h1>
-//         <form action="/signin" method="post">
-//             <p>Name: <input name="name" value="koa"></p>
-//             <p>Password: <input name="password" type="password"></p>
-//             <p><input type="submit" value="Submit"></p>
-//         </form>`;
-// });
-
-
-// router.post('/signin', async (ctx, next) => {
-//     var name = ctx.request.body.name || '',
-//         password = ctx.request.body.password || '';
-//     console.log(`signin with name: ${name}, password: ${password}`);
-//     if (name === 'koa' && password === '12345') {
-//         ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-//     } else {
-//         ctx.response.body = `<h1>Login failed!</h1>
-//         <p><a href="/">Try again</a></p>`;
-//     }
-// });
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function(n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
 
 
 // 使用middleware:
