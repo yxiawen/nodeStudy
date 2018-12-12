f247767a0dabd43f4a9b955e72b3ba1ccde3801c
yxiawen <yxiawen@126.com>
2018-12-12 15:23:15 +0800

    12.12 配置日志文件

--- testKoa/config/log_config.js
+++ testKoa/config/log_config.js
@@ -1,55 +1,68 @@
+var path = require('path');
+
+//日志根目录
+var baseLogPath = path.resolve(__dirname, '../logs')
+
+//错误日志目录
+var errorPath = "/error";
+//错误日志文件名
+var errorFileName = "error";
+//错误日志输出完整路径
+var errorLogPath = baseLogPath + errorPath + "/" + errorFileName;
+//var errorLogPath = path.resolve(__dirname, "../logs/error/error");
+
+//响应日志目录
+var responsePath = "/response";
+//响应日志文件名
+var responseFileName = "response";
+//响应日志输出完整路径
+var responseLogPath = baseLogPath + responsePath + "/" + responseFileName;
+//var responseLogPath = path.resolve(__dirname, "../logs/response/response");
 module.exports = {
-  "appenders": {
-    "access": {
-      "type": "dateFile",
-      "filename": "logs/access.log",
-      "pattern": "-yyyy-MM-dd"
+  //日志格式等设置
+  appenders: {
+    "rule-console": {
+      "type": "console"
     },
-    // "rule-console": {
-    //   "type": "console"
-    // },
-    "rule-response": {
+    "errorLogger": {
       "type": "dateFile",
-      "filename": "logs/response/server-",
+      "filename": errorLogPath,
+      "pattern": "-yyyy-MM-dd-hh.log",
+      "alwaysIncludePattern": true,
       "encoding": "utf-8",
-      "maxLogSize": 10000000,
+      "maxLogSize": 10000,
       "numBackups": 3,
-      "pattern": "yyyy-MM-dd.log",
-      "alwaysIncludePattern": true
+      "path": errorPath
     },
-    "rule-error": {
+    "resLogger": {
       "type": "dateFile",
-      "filename": "logs/error/error-",
+      "filename": responseLogPath,
+      "pattern": "-yyyy-MM-dd-hh.log",
+      "alwaysIncludePattern": true,
       "encoding": "utf-8",
-      "maxLogSize": 1000000,
+      "maxLogSize": 10000,
       "numBackups": 3,
-      "pattern": "yyyy-MM-dd.log",
-      "alwaysIncludePattern": true
+      "path": responsePath
     },
   },
-  "categories": {
-    "rule-response": {
-      "appenders": ['rule-response'],
-      "level": 'info'
-    },
-    "rule-error": {
-      "appenders": ['rule-error'],
-      "level": 'error'
-    },
+  //供外部调用的名称和对应设置定义
+  categories: {
     "default": {
-      "appenders": [
-        // "rule-console",
-        "rule-response",
-        "rule-error"
-      ],
-      "level": "trace"
+      "appenders": ["rule-console"],
+      "level": "all"
     },
-
+    "resLogger": {
+      "appenders": ["resLogger"],
+      "level": "info"
+    },
+    "errorLogger": {
+      "appenders": ["errorLogger"],
+      "level": "error"
+    },
     "http": {
-      "appenders": [
-        "access"
-      ],
+      "appenders": ["resLogger"],
       "level": "info"
     }
-  }
+  },
+  "baseLogPath": baseLogPath
 }
